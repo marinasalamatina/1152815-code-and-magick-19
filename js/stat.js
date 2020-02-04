72cafca0380a3310bf2ec2755108f949acde17ec
marinasalamatina <marinasalamatina@mail.ru>
2020-02-04 15:25:03 +0300

    Add -getRenderBar-, move vars

--- js/stat.js
+++ js/stat.js
@@ -6,7 +6,7 @@ var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var CLOUD_COLOR = '#ffffff';
 var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
-var CLOUD_GUP = 5;
+var CLOUD_GAP = 5;
 
 var CLOUD_PADDING_X = 20;
 var CLOUD_PADDING_Y = 30;
@@ -23,21 +23,13 @@ var FONT_FAMILY = '16px PT Mono';
 var HUE = 240;
 var LIGHTNESS = 50;
 
-var shadowX = CLOUD_X + CLOUD_GUP;
-var shadowY = CLOUD_Y + CLOUD_GUP;
-
-var shadowWidth = CLOUD_WIDTH + CLOUD_GUP;
-var shadowHeight = CLOUD_HEIGHT + CLOUD_GUP;
-
-var headerX = CLOUD_X + CLOUD_PADDING_X;
-var headerY = CLOUD_Y + CLOUD_PADDING_Y;
-
-var stringPadding = headerY + TEXT_HEIGHT;
-var marginBottom = BAR_PADDING + BAR_HEIGHT_MAX;
-var barWidthMargin = BAR_WIDTH + BAR_GAP;
-
-
 var getRenderCloud = function (ctx, x, y) {
+  var shadowX = CLOUD_X + CLOUD_GAP;
+  var shadowY = CLOUD_Y + CLOUD_GAP;
+
+  var shadowWidth = CLOUD_WIDTH + CLOUD_GAP;
+  var shadowHeight = CLOUD_HEIGHT + CLOUD_GAP;
+
   ctx.fillStyle = SHADOW_COLOR;
   ctx.fillRect(shadowX, shadowY, shadowWidth, shadowHeight);
   ctx.fillStyle = CLOUD_COLOR;
@@ -46,35 +38,46 @@ var getRenderCloud = function (ctx, x, y) {
 
 
 var getRenderHeader = function (ctx, x, y) {
+  var headerX = x + CLOUD_PADDING_X;
+  var headerY = y + CLOUD_PADDING_Y;
+  var stringPadding = headerY + TEXT_HEIGHT;
+
   ctx.fillStyle = TEXT_COLOR;
   ctx.font = FONT_FAMILY;
-  ctx.fillText('Ура вы победили!', x, y);
+  ctx.fillText('Ура вы победили!', headerX, headerY);
   ctx.fillText('Список результатов:', headerX, stringPadding);
 };
 
-var getRenderColor = function () {
-  var saturation = Math.round(Math.random() * 100);
-
-  return 'hsl(' + HUE + ', ' + saturation + '%, ' + LIGHTNESS + '%)';
-};
-
-var getMaxValue = function (array) {
-  var maxValue = array[0];
-
-  for (var i = 0; i < array.length; i += 1) {
-    if (array[i] > maxValue) {
-      maxValue = array[i];
-    }
-  }
-
-  return maxValue;
+var getRenderBar = function (ctx, x, y, width, height, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, width, height, color);
 };
 
 var getRenderBars = function (ctx, times, names) {
+  var marginBottom = BAR_PADDING + BAR_HEIGHT_MAX;
+  var barWidthMargin = BAR_WIDTH + BAR_GAP;
 
   var getBarColor = function () {
     var barColor = (names[i] === 'Вы') ? PLAYER_BAR_COLOR : getRenderColor();
     return barColor;
+  };
+
+  var getRenderColor = function () {
+    var saturation = Math.round(Math.random() * 100);
+
+    return 'hsl(' + HUE + ', ' + saturation + '%, ' + LIGHTNESS + '%)';
+  };
+
+  var getMaxValue = function (array) {
+    var maxValue = array[0];
+
+    for (var i = 0; i < array.length; i += 1) {
+      if (array[i] > maxValue) {
+        maxValue = array[i];
+      }
+    }
+
+    return maxValue;
   };
 
   for (var i = 0; i < times.length; i += 1) {
@@ -89,13 +92,12 @@ var getRenderBars = function (ctx, times, names) {
     ctx.fillText(Math.round(times[i]), barX, timeY);
     ctx.fillText(names[i], barX, nameY);
 
-    ctx.fillStyle = getBarColor();
-    ctx.fillRect(barX, barY, BAR_WIDTH, certainHeight);
+    getRenderBar(ctx, barX, barY, BAR_WIDTH, certainHeight, getBarColor());
   }
 };
 
 window.renderStatistics = function (ctx, names, times) {
   getRenderCloud(ctx, CLOUD_X, CLOUD_Y);
-  getRenderHeader(ctx, headerX, headerY);
+  getRenderHeader(ctx, CLOUD_X, CLOUD_Y);
   getRenderBars(ctx, times, names);
 };
